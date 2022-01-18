e407a337c48c3674e5a68d9b1fef803346d405ee
Hiroshi KATAOKA <e205745@ie.u-ryukyu.ac.jp>
2022-01-19 02:07:49 +0900

    Progress2

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,5 +1,8 @@
 import jp.ac.uryukyu.ie.e205745.*;
 
 public class Main {
-    
+    public static void main(String[] args){
+        Adult adult = new Adult("おとな");
+        Kids kids = new Kids("こども");
+    }
 }
